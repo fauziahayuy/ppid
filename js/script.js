4388b9d3e79ae0ace3de9dc4e2f7913e0b4ef16d
fauziahayuy <fauziahayu990@gmail.com>
2023-12-21 09:32:42 +0800

    update set char title kegiatan

--- js/script.js
+++ js/script.js
@@ -9,3 +9,28 @@ document.addEventListener("DOMContentLoaded", function () {
         this.classList.remove("focused");
     });
 });
+
+document.ready(function(){
+    $(".owl-carousel-award").owlCarousel({
+        autoplay: true,
+        autoplayhoverpause: true,
+        autoplaytimeout: 200,
+        items: 2,
+        nav: false,
+        loop: true,
+        responsive:{
+          0:{
+            items:1
+          },
+          600:{
+           items:1,
+          },
+          800:{
+            items:3,
+          },
+          1200:{
+            items:3,
+          }    
+        }
+    });
+})
